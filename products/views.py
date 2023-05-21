7ff63ac941c3b08c3cd21656b69d42fd9ac4a137
mutkovicova <>
2023-05-21 12:34:22 +0000

    added product views and categories in admin

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A view to return tall products, including sorting and search """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
