eb505f3e9d87589e0668ea1bfbc325491253f0a0
mutkovicova <>
2023-05-28 14:27:22 +0000

    significant update - had to go back in walkthrough and apply sizes

--- products/models.py
+++ products/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
