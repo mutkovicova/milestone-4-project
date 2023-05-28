73974356460ec5cc01ead059faaac6d3a1780d4e
mutkovicova <>
2023-05-28 17:40:21 +0000

    started checkout app views file

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Oh no, you don't have anything to buy!")
+        return redirect(reverse('products'))
