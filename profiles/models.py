098a640201181ae1f9e5a79c8bfd4fc4de41981e
mutkovicova <>
2023-05-29 23:31:07 +0000

    created profile and updated base templates

--- profiles/models.py
+++ profiles/models.py
@@ -24,6 +24,6 @@ class UserProfile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
 
     if created:
-        UserProfile.objects.create(user=instance)
+      UserProfile.objects.create(user=instance)
 
     instance.userprofile.save()
