eec4d8103a90733eb242f2d442bc96c165f4b812
mutkovicova <>
2023-05-29 20:20:42 +0000

    webhook finalised setup

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -13,7 +13,8 @@ class StripeWH_Handler:
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
-
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
