c173f29bbe1ceb6f3bf1b566751c5d615b372c8d
mutkovicova <>
2023-05-29 18:57:11 +0000

    checkout functionality improved and tested

--- checkout/models.py
+++ checkout/models.py
@@ -31,7 +31,7 @@ class Order(models.Model):
 
     def update_total(self):
 
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
--- checkout/signals.py
+++ checkout/signals.py
@@ -11,6 +11,6 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
 
     instance.order.update_total()
