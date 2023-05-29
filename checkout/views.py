1ad87caa073d410ffc51be99ca4b0346c7ebb8df
mutkovicova <>
2023-05-29 18:03:09 +0000

    stripe keys and configuration of actions

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Oh no, you don't have anything to buy!")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
