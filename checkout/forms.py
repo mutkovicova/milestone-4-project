1ee0188fffd1f0456a328b6c262ecbf7bed05c54
mutkovicova <>
2023-05-29 23:55:28 +0000

    profile page form complete

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,11 +20,11 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'postcode': 'Postal Code',
+            'postcode': 'Postcode',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County, State or Locality',
+            'county': 'County',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
