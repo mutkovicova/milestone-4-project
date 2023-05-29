3529b86895bc6b1953208845809bf00f49a03c51
mutkovicova <>
2023-05-29 20:02:59 +0000

    created and tested webhooks

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_failed(self, event):
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
