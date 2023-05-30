4f968f3734fa056f3ac88a380e92948795501daf
mutkovicova <>
2023-05-30 14:37:57 +0000

    allowed hosts check

--- slovak_shop/settings.py
+++ slovak_shop/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-=ef(caw&7@0on%)w#emroev+we!fuvq*wunu+qb&oyoj5_^4ld
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'slovak-shop-milestone-4.herokuapp.com/',
+    'slovak-shop-milestone-4.herokuapp.com',
     'localhost',
     '8000-mutkovicova-milestone-4-an8dk80zyz.us2.codeanyapp.com',
     ]
