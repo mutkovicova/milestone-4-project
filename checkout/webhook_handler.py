7cf9b1ec9ddddbd013ff78904f70765f4baa185b
Maggie M <55878536+mutkovicova@users.noreply.github.com>
2023-07-08 16:25:14 +0100

    fixing stripe error in webhook handler file

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -46,13 +46,9 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        stripe_charge = stripe.Charge.retrieve(
-            intent.latest_charge
-        )
-
-        billing_details = stripe_charge.billing_details
+        billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2)
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         for field, value in shipping_details.address.items():
             if value == "":
