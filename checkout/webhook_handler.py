68d8b19c9753bed6aa31b0a9334e27d2f3159481
Maggie M <55878536+mutkovicova@users.noreply.github.com>
2023-07-08 18:10:35 +0100

    confirm emails url change

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -20,10 +20,10 @@ class StripeWH_Handler:
         """Send the user a confirmation email"""
         cust_email = order.email
         subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            'templates/checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
         body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
+            'templates/checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
         
         send_mail(
