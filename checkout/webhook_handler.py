93f09f1e06bb9f1e454246192e2243db593dc5e2
Maggie M <55878536+mutkovicova@users.noreply.github.com>
2023-07-08 16:17:19 +0100

    email troubleshooting

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -20,10 +20,10 @@ class StripeWH_Handler:
 
         cust_email = order.email
         subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            'templates/checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
         body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
+            'templates/checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
         
         send_mail(
