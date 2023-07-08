290bb75ebab05655ad17a0f27e78fe4e46f0b76d
Maggie M <55878536+mutkovicova@users.noreply.github.com>
2023-07-08 17:02:28 +0100

    troubleshooting emails stripe WH

--- slovak_shop/settings.py
+++ slovak_shop/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=ef(caw&7@0on%)w#emroev+we!fuvq*wunu+qb&oyoj5_^4ld'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
