a6f9af5025b419ecbfb9f0525b9f33007ee2ce73
mutkovicova <slovakgal@gmail.com>
2023-08-20 19:23:02 +0000

    undo contactus form attempt

--- slovak_shop/urls.py
+++ slovak_shop/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('faq/', include('faq.urls')),
-    path('contactus', include('contactus.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
